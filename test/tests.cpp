6863d20a8e818d29866e0a65d445f1513d7c0f5f
Dmitry <xtopz1337@gmail.com>
2024-03-02 21:23:39 +0300

    refactor: add copyright

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,4 @@
-
+// Copyright 2024 Dmitry
 #include <gtest/gtest.h>
 #include <cstdint>
 #include "alg.h"
@@ -19,12 +19,12 @@ TEST(primary_numbers, check_high_numbers_on_primary) {
     ASSERT_FALSE(checkPrime(UINT64_MAX - 1));
 }
 
-//TEST(primary_numbers, zero_in_a_primary_row_number) {
-//    // Вопрос в нумерации ряда простых чисел.
-//    // По нулевой позиции - результат 0, что ошибочно, 0 - непростое
-//    // Тест не выполняется!
-//    ASSERT_NE(nPrime(0), 0);
-//}
+// TEST(primary_numbers, zero_in_a_primary_row_number) {
+//     // Вопрос в нумерации ряда простых чисел.
+//     // По нулевой позиции - результат 0, что ошибочно, 0 - непростое
+//     // Тест не выполняется!
+//     ASSERT_NE(nPrime(0), 0);
+// }
 
 TEST(primary_numbers, primary_row_numbers) {
     ASSERT_EQ(nPrime(5), 11);
@@ -65,4 +65,3 @@ TEST(primary_numbers, sum_primary) {
 TEST(primary_numbers, sum_primary_until_two_millions_bound) {
     ASSERT_EQ(sumPrime(2000000), 142913828922);
 }
-
